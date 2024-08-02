bd2949c4ad412c020107c1deb74e893cd4259c83
Admin <Admin@ADMIN>
2024-08-02 17:13:35 +0530

    test

---
+++ src/test/java/Sample/Addition.java
@@ -0,0 +1,15 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Addition
+{
+	@Test
+	public void Add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
